780b0713f788526152e76d02a2ad7c3b3ccc7eec
ginn cheng <ginn.z.cheng@gmail.com>
2024-06-03 19:04:12 +0800

    refactor: refactor the model python script

--- sparkify_etl_model.py
+++ sparkify_etl_model.py
@@ -21,127 +21,138 @@ from pyspark.sql import DataFrame
 from pyspark.sql.types import *
 from pyspark.ml.util import DefaultParamsReadable, DefaultParamsWritable
 
+file_path = "mini_sparkify_event_data.json"
+output_path = './bestModel'
 # create a Spark session
 spark = SparkSession.builder \
     .appName("Sparkify Data Analysis") \
     .getOrCreate()
 
-file_path = "mini_sparkify_event_data.json"
-# read the data via spark
-sc = spark.read.json(file_path)
-output_path = './bestModel'
+class DataWranglingTransformer(Transformer, DefaultParamsReadable, DefaultParamsWritable):
+    def __init__(self, file_path = 'mini_sparkify_event_data.json',
+                       output_path = './bestModel'):
+        super(DataWranglingTransformer, self).__init__()
+        self.file_path = file_path
+        self.output_path = output_path
 
-# Convert data types
-columns_to_cast = {
-    "userId": "Integer",
-    "length": "Double"
-}
+    def _transform(self, sc: DataFrame) -> DataFrame:
 
-for column, dtype in columns_to_cast.items():
-    sc = sc.withColumn(column, sc[column].cast(dtype))
+        # read the data via spark
+        sc = spark.read.json(self.file_path)
 
-# compute the mean length for each userID
-mean_length = sc.groupBy('userId').agg(avg('length').alias('mean_length'))
+        # Convert data types
+        columns_to_cast = {
+            "userId": "Integer",
+            "length": "Double"
+        }
 
-# join the mean lengths back to the original DataFrame
-sc_length = sc.join(mean_length, on='userId', how='left')
+        for column, dtype in columns_to_cast.items():
+            sc = sc.withColumn(column, sc[column].cast(dtype))
 
-# fill null values
-sc_filled = sc_length.withColumn(
-    'length_filled',when(sc_length['length'].isNull(), sc_length['mean_length']).otherwise(sc_length['length'])
-)
-# drop columns and rename the length_filled to length
-sc_filled = sc_filled.drop('length','mean_length').withColumnRenamed('length_filled', 'length')
+        # compute the mean length for each userID
+        mean_length = sc.groupBy('userId').agg(avg('length').alias('mean_length'))
 
-sc = sc_filled
+        # join the mean lengths back to the original DataFrame
+        sc_length = sc.join(mean_length, on='userId', how='left')
 
-# rename the column names
-to_lower_coln = udf(lambda x: x.lower(), StringType())
+        # fill null values
+        sc_filled = sc_length.withColumn(
+            'length_filled', when(sc_length['length'].isNull(), sc_length['mean_length']).otherwise(sc_length['length'])
+        )
+        # drop columns and rename the length_filled to length
+        sc_filled = sc_filled.drop('length', 'mean_length').withColumnRenamed('length_filled', 'length')
 
-for col in sc.columns:
-    sc.withColumn(col, to_lower_coln(col))
+        sc = sc_filled
 
-# select the relevant columns
-coln_selected = ['gender', 'itemInSession', 'level', 'location', 'length', 'method', 'page', 'sessionId', 'status', 'ts', 'userId']
-sc_sl = sc.select(coln_selected)
+        # rename the column names
+        to_lower_coln = udf(lambda x: x.lower(), StringType())
 
-# drop na and duplicated
-sc_cl = sc_sl.dropna()
-sc_cl = sc_cl.dropDuplicates()
+        for col in sc.columns:
+            sc.withColumn(col, to_lower_coln(col))
 
-# add a column of churn
-to_label_churn = udf(lambda x: 2 if x == 'Cancellation Confirmation' else 1 if x == 'Downgrade' else 0)
-sc_cl = sc_cl.withColumn('churn', to_label_churn(sc_cl.page))
+        # select the relevant columns
+        coln_selected = ['gender', 'itemInSession', 'level', 'location', 'length', 'method', 'page', 'sessionId',
+                         'status', 'ts', 'userId']
+        sc_sl = sc.select(coln_selected)
 
-# convert time
-get_year = udf(lambda x: datetime.datetime.fromtimestamp(x / 1000.0). year)
-get_month = udf(lambda x: datetime.datetime.fromtimestamp(x / 1000.0). month)
-get_day = udf(lambda x: datetime.datetime.fromtimestamp(x / 1000.0). day)
-get_hour = udf(lambda x: datetime.datetime.fromtimestamp(x / 1000.0). hour)
+        # drop na and duplicated
+        sc_cl = sc_sl.dropna()
+        sc_cl = sc_cl.dropDuplicates()
 
-sc_cl = sc_cl.withColumn('year', get_year(sc_cl.ts))
-sc_cl = sc_cl.withColumn('month', get_month(sc_cl.ts))
-sc_cl = sc_cl.withColumn('day', get_day(sc_cl.ts))
-sc_cl = sc_cl.withColumn('hour', get_hour(sc_cl.ts))
+        # add a column of churn
+        to_label_churn = udf(lambda x: 2 if x == 'Cancellation Confirmation' else 1 if x == 'Downgrade' else 0)
+        sc_cl = sc_cl.withColumn('churn', to_label_churn(sc_cl.page))
 
-sc_cl = sc_cl.drop('ts')
+        # convert time
+        get_year = udf(lambda x: datetime.datetime.fromtimestamp(x / 1000.0).year)
+        get_month = udf(lambda x: datetime.datetime.fromtimestamp(x / 1000.0).month)
+        get_day = udf(lambda x: datetime.datetime.fromtimestamp(x / 1000.0).day)
+        get_hour = udf(lambda x: datetime.datetime.fromtimestamp(x / 1000.0).hour)
 
-# convert the data types
-columns_to_cast = {
-    'churn': 'Integer',
-    'year': 'Integer',
-    'month': 'Integer',
-    'day': 'Integer',
-    'hour': 'Integer'
-}
+        sc_cl = sc_cl.withColumn('year', get_year(sc_cl.ts))
+        sc_cl = sc_cl.withColumn('month', get_month(sc_cl.ts))
+        sc_cl = sc_cl.withColumn('day', get_day(sc_cl.ts))
+        sc_cl = sc_cl.withColumn('hour', get_hour(sc_cl.ts))
 
-for column, dtype in columns_to_cast.items():
-    sc_cl = sc_cl.withColumn(column, sc_cl[column].cast(dtype))
+        sc_cl = sc_cl.drop('ts')
 
-# select on the state as the location
-to_extract_state = udf(lambda s: s.split(',')[1].strip() if ',' in s else s)
+        # convert the data types
+        columns_to_cast = {
+            'churn': 'Integer',
+            'year': 'Integer',
+            'month': 'Integer',
+            'day': 'Integer',
+            'hour': 'Integer'
+        }
 
-sc_cl = sc_cl.withColumn('state', to_extract_state(sc_cl.location))
+        for column, dtype in columns_to_cast.items():
+            sc_cl = sc_cl.withColumn(column, sc_cl[column].cast(dtype))
 
+        # select on the state as the location
+        to_extract_state = udf(lambda s: s.split(',')[1].strip() if ',' in s else s)
 
-sc_cl2 = sc_cl.withColumn('states', split(sc_cl.state, "-"))
-sc_cl2 = sc_cl2.withColumn('state', explode(sc_cl2.states))
-sc_cl = sc_cl2.drop('states','location')
+        sc_cl = sc_cl.withColumn('state', to_extract_state(sc_cl.location))
 
-# indexing the strings
-# select the relevant columns
-coln_selected = ['gender', 'itemInSession', 'level', 'length', 'churn']
-sc_cl = sc_cl.select(coln_selected)
+        sc_cl2 = sc_cl.withColumn('states', split(sc_cl.state, "-"))
+        sc_cl2 = sc_cl2.withColumn('state', explode(sc_cl2.states))
+        sc_cl = sc_cl2.drop('states', 'location')
 
-for col in sc_cl.columns:
-    if sc_cl.select(col).dtypes[0][1] == 'string':
-        print(col,':',sc_cl.select(col).dtypes[0][1])
-        indexer = StringIndexer(inputCol=col, outputCol=col + "_index")
-        sc_cl = indexer.fit(sc_cl).transform(sc_cl)
-        sc_cl = sc_cl.drop(col).withColumnRenamed(col + "_index", col)
+        # indexing the strings
+        # select the relevant columns
+        coln_selected = ['gender', 'itemInSession', 'level', 'length', 'churn']
+        sc_cl = sc_cl.select(coln_selected)
 
+        for col in sc_cl.columns:
+            if sc_cl.select(col).dtypes[0][1] == 'string':
+                print(col, ':', sc_cl.select(col).dtypes[0][1])
+                indexer = StringIndexer(inputCol=col, outputCol=col + "_index")
+                sc_cl = indexer.fit(sc_cl).transform(sc_cl)
+                sc_cl = sc_cl.drop(col).withColumnRenamed(col + "_index", col)
 
-# Calculate the number of instances for each class
-class_counts = sc_cl.groupBy('churn').count().collect()
-churn_counts = [class_counts[0][1],class_counts[1][1],class_counts[2][1]]
-max_count = max(churn_counts)
-min_count = min(churn_counts)
-mid_count = [x for x in churn_counts if x not in [max_count, min_count]][0]
+        # Calculate the number of instances for each class
+        class_counts = sc_cl.groupBy('churn').count().collect()
+        churn_counts = [class_counts[0][1], class_counts[1][1], class_counts[2][1]]
+        max_count = max(churn_counts)
+        min_count = min(churn_counts)
+        mid_count = [x for x in churn_counts if x not in [max_count, min_count]][0]
 
-# Calculate weights
-mid_ratio = 2*min_count/mid_count
-max_ratio = 2*min_count/max_count
+        # Calculate weights
+        mid_ratio = 2 * min_count / mid_count
+        max_ratio = 2 * min_count / max_count
 
-# select the data based on the churns
-churn_0 = sc_cl.where(sc_cl.churn == 0).sample(withReplacement=False, fraction=max_ratio, seed=42)
-churn_1 = sc_cl.where(sc_cl.churn == 1).sample(withReplacement=False, fraction=mid_ratio, seed=42)
-churn_2 = sc_cl.where(sc_cl.churn == 2)
-# merge the data
-sc_cl = churn_0.union(churn_1.union(churn_2))
+        # select the data based on the churns
+        churn_0 = sc_cl.where(sc_cl.churn == 0).sample(withReplacement=False, fraction=max_ratio, seed=42)
+        churn_1 = sc_cl.where(sc_cl.churn == 1).sample(withReplacement=False, fraction=mid_ratio, seed=42)
+        churn_2 = sc_cl.where(sc_cl.churn == 2)
+        # merge the data
+        sc_cl = churn_0.union(churn_1.union(churn_2))
 
-# Split the data into training and testing sets
-train_data, rest_data = sc_cl.randomSplit([0.7, 0.3], seed=42)
-valid_data, test_data = rest_data.randomSplit([0.5, 0.5], seed=42)
+        # Split the data into training and testing sets
+        train_data, rest_data = sc_cl.randomSplit([0.7, 0.3], seed=42)
+        valid_data, test_data = rest_data.randomSplit([0.5, 0.5], seed=42)
+
+        # Return the transformed DataFrame
+        return train_data, valid_data, test_data
 
 
 class sparkify_model:
@@ -159,7 +170,7 @@ class sparkify_model:
         # Define the classifier
         rf = RandomForestClassifier(featuresCol='features', labelCol=self.label, predictionCol='churn_pred')
         # Create the pipeline
-        pipeline = Pipeline(stages=[assembler, rf])
+        pipeline = Pipeline(stages=[DataWranglingTransformer,assembler, rf])
         # Set Up Cross-Validation
         # Define the parameter grid
         paramGrid = ParamGridBuilder() \
